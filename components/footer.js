f3a62d6391cafe887794c7a093f56f164ee2c179
Shahzeb Malik <shahzebmalik00@gmail.com>
2022-10-18 09:32:18 -0700

    padding updates

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ function Footer() {
             <img className={style.footer_logo} src="logo-white.svg" alt="" />
             <Line />
             <p className={"paragraph light" + " " + style.footer_text}>
-              YourProductMatters Inc is a full-service digital agency with a
+              YourProductMatters is a full-service digital agency with a
               passion for ingenuity and innovation in every medium, YourProductMatters focuses on expanding its digital expertise to bring you
               the best solutions. We are happy and proud to have in our
               portfolio a large base of satisfied clients and success stories.
