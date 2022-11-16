6c51520cf811f1b3964ebabf1f7f3a31a55d8cf9
Haris9922 <118390305+Haris9922@users.noreply.github.com>
2022-11-17 03:42:55 +0500

    Update integratedSolutions.jsx

--- components/landingpagecomponents/integratedSolution/integratedSolutions.jsx
+++ components/landingpagecomponents/integratedSolution/integratedSolutions.jsx
@@ -9,7 +9,7 @@ const IntegratedSolutions = () => {
             <div className={`${styles.container}`}>
                 <div className={`${styles.row}`}>
                     <div className={`${styles.col_md_12} ${styles.text_center}`}>
-                        <h2 className={`subheading-2`}>Integrated solutions designed for small business</h2>
+                        <h2 className={`subheading-2`}>What you can expect from us</h2>
                         <p className={`${styles.section_subtitle}`}>Communicate with flexible tools that go where your team goes.</p>
                     </div>
                 </div>
@@ -20,22 +20,22 @@ const IntegratedSolutions = () => {
                         <div className={`${styles.col_md_4}`}>
                             <div className={`${styles.main_services}`}>
                                 <img src="/lp2/services-icon1.png" alt="pic"/>
-                                <h3>Designers</h3>
-                                <p>Curabitur quam etsum lacus net netsum nulat iaculis etsimun vitae etsum nisle varius sed aliquam etsim vitae netsum.</p>
+                                <h3>Technical expertise</h3>
+                                <p>Our technical team is divided into multiple departments based on the technology they work on. These departments are led by individuals that are veterans of their craft.</p>
                             </div>
                         </div>
                         <div className={`${styles.col_md_4}`}>
                             <div className={`${styles.main_services}`}>
                                 <img src="/lp2/services-icon2.png" alt="pic"/>
-                                <h3>Marketers</h3>
-                                <p>Curabitur quam etsum lacus net netsum nulat iaculis etsimun vitae etsum nisle varius sed aliquam etsim vitae netsum.</p>
+                                <h3>Clear communication</h3>
+                                <p>We believe that a clear channel of communication is as important as your knowledge. Your project manager will keep you in the loop every step of the way to view website/app progress, provide input, and allow you to voice any comments or concerns.</p>
                             </div>
                         </div>
                         <div className={`${styles.col_md_4}`}>
                             <div className={`${styles.main_services}`}>
                                 <img src="/lp2/services-icon3.png" alt="pic" />
-                                <h3>Agencies</h3>
-                                <p>Curabitur quam etsum lacus net netsum nulat iaculis etsimun vitae etsum nisle varius sed aliquam etsim vitae netsum.</p>
+                                <h3>Commitment </h3>
+                                <p>OUR WORD IS OUR BOND! We stick to the budget and the time that we have quoted!</p>
                             </div>
                         </div>
                     </div>
