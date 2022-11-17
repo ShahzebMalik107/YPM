1f6e6faf38e21b90a25fc4159404fa236e1de55c
Haris9922 <118390305+Haris9922@users.noreply.github.com>
2022-11-18 01:15:06 +0500

    Update banner.jsx

--- components/landingpagecomponents/banner/banner.jsx
+++ components/landingpagecomponents/banner/banner.jsx
@@ -15,19 +15,16 @@ const Banner = () => {
             <p>
             Whether your idea is about design or development, we’ll connect you with an expert so you can talk about your project. We’ll also share a quote with you. Here’s the best part, IT’S FREE!
             </p>
-            {/* <div id="call" className={styles.lp_form}>
+            <div id="call" className={styles.lp_form}>
               <Customform />
-            </div> */}
+            </div>
           </div>
-          {/* <div className={styles.banner_img}>
+          <div className={styles.banner_img}>
             <img
               src="https://images.unsplash.com/photo-1665891098760-01d45ddb0e4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
               alt="bi"
             />
-          </div> */}
-          <div id="call" className={styles.lp_form}>
-              <Customform />
-            </div>
+          </div>
         </div>
       </div>
     </section>
